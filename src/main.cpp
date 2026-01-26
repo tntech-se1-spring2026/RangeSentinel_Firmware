98132e95b6eac15cbc0923e552f11339272320dd
Andrew Johnson <andrewjohnson11235@gmail.com>
2026-01-26 17:31:12 -0600

    feat: Add function to update databse in memory

--- src/main.cpp
+++ src/main.cpp
@@ -14,9 +14,26 @@ void loop() {
 
 #endif
 
+// ----------------------------------
 
 // viewing node
 #ifdef NODE_TYPE_VIEWER
+
+#define MAX_NODES 10
+NodeStatus networkDatabase[MAX_NODES] = {0};
+
+// helper function to update database
+void updateDatabase(NodeStatus incoming) {
+    if (incoming.nodeId >= MAX_NODES) {
+        return;
+    }
+
+    // only update if incoming is newer than what is already there
+    if (incoming.messageId > networkDatabase[incoming.nodeId].messageId) {
+        networkDatabase[incoming.nodeId] = incoming;
+        Serial.printf("DB: Node %d updated (Msg %d)\n", incoming.nodeId, incoming.messageId);
+    }
+}
 // may need more includes here for viewer since it interacts with web server
 
 void setup() {
