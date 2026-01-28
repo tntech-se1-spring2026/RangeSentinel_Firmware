e4334d61df3cd40acc8ecee2cd6f542463aaf947
Andrew Johnson <andrewjohnson11235@gmail.com>
2026-01-27 21:04:19 -0600

    feat: implement wifi gateway access point and JSON status API

--- src/main.cpp
+++ src/main.cpp
@@ -16,11 +16,17 @@ void loop() {
 #endif
 
 // ----------------------------------
-
+//TEMPORARY
+#define NODE_TYPE_VIEWER
 // viewing node
 #ifdef NODE_TYPE_VIEWER
+#include <WiFi.h>
+#include <WebServer.h>
 
 #define MAX_NODES 10
+
+// standard HTTP port
+WebServer server(80);
 NodeStatus networkDatabase[MAX_NODES] = {0};
 
 // helper function to update database
@@ -41,7 +47,7 @@ String getDatabaseAsJson() {
     JsonDocument doc;
     JsonArray root = doc.to<JsonArray>();
 
-    for (int i = 0; i < MAX_NODES, i++) {
+    for (int i = 0; i < MAX_NODES; i++) {
         if (networkDatabase[i].messageId > 0) {
             JsonObject obj = root.add<JsonObject>();
             obj["id"] = networkDatabase[i].nodeId;
@@ -58,11 +64,28 @@ String getDatabaseAsJson() {
 }
 
 void setup() {
+    Serial.begin(115200);
 
+    // start access point
+    // (SSID, Password)
+    WiFi.softAP("Range-Sentinel-Gateway", "secure-sentinel-2026");
+    Serial.print("Gateway IP Address: ");
+    Serial.println(WiFi.softAPIP());  // should default to 192.168.4.1
+
+    // home page
+    server.on("/", HTTP_GET, []() {
+        server.send(200, "text/html", "<h1>Range Sentinel</h1><p>API is online!!!</p>");
+    });
+
+    // JSON data api
+    server.on("/api/status", HTTP_GET, []() {
+        server.send(200, "application/json", getDatabaseAsJson());
+    });
+    server.begin();
 }
 
 void loop() {
-
+    server.handleClient();
 }
 
 #endif
