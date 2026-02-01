8ee850bf31dca9ab364406780c182073e41f6326
Andrew Johnson <andrewjohnson11235@gmail.com>
2026-02-01 17:30:36 -0600

    refactor: Abstract some cluttered viewing node logic outside of main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <array>
 #include "shared_types.h"
 #include "web_server.h"
+#include "database_manager.h"
 
 // sensor node
 #ifdef NODE_TYPE_SENSOR
@@ -24,50 +25,9 @@ void loop() {
 #include <WebServer.h>
 #include <LittleFS.h>
 
-#define MAX_NODES 10
-
 // standard HTTP port
 WebServer server(80);
 std::array<NodeStatus, MAX_NODES> networkDatabase = {};
-
-// helper function to update database
-void updateDatabase(NodeStatus incoming) {
-    if (incoming.nodeId < networkDatabase.size()) {
-        // only update if incoming is newer than what is already there
-        if (incoming.messageId > networkDatabase.at(incoming.nodeId).messageId) {
-            networkDatabase.at(incoming.nodeId) = incoming;
-            Serial.printf("DB: Node %d updated (Msg %d)\n", incoming.nodeId, incoming.messageId);
-        } 
-        else {
-            Serial.printf("DB: Ignored old msg %d from node %d\n", incoming.messageId, incoming.nodeId);
-        }
-    }
-    else {
-        Serial.printf("DB: Rejected node %d (out of bounds)\n", incoming.nodeId);
-    }
-}
-
-// converts entire active database to JSON array
-String getDatabaseAsJson() {
-    JsonDocument doc;
-    JsonArray root = doc.to<JsonArray>();
-
-    for (const auto& node : networkDatabase) {
-        if (node.messageId > 0) {
-            JsonObject obj = root.add<JsonObject>();
-            obj["id"] = node.nodeId;
-            obj["mId"] = node.messageId;
-            obj["batt"] = node.batteryVoltage;
-            obj["motion"] = node.motionDetected;
-            obj["door"] = node.doorOpen;
-            obj["name"] = String(node.nodeName);
-        }
-    }
-
-    String output;
-    serializeJson(doc, output);
-    return output;
-}
 
 void setup() {
     Serial.begin(115200);
