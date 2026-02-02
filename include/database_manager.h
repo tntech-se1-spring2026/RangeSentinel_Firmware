525dc0c2a2ecdfdf5850bb38cdc082144db5d52e
Andrew Johnson <andrewjohnson11235@gmail.com>
2026-02-01 22:02:30 -0600

    feat: Add function to retrieve data from our backup database

--- include/database_manager.h
+++ include/database_manager.h
@@ -2,11 +2,15 @@
 #define DATABASE_MANAGER_H
 
 #include <array>
+#include <LittleFS.h>
 #include "shared_types.h"
 
 #define MAX_NODES 10
 // officially declared in main.cpp
 extern std::array<NodeStatus, MAX_NODES> networkDatabase;
+
+// global flag to track if we need to add to persistent memory
+inline bool needsPersistence = false;
 
 // helper function to update database
 inline void updateDatabase(NodeStatus incoming) {
@@ -14,6 +18,7 @@ inline void updateDatabase(NodeStatus incoming) {
         // only update if incoming is newer than what is already there
         if (incoming.messageId > networkDatabase.at(incoming.nodeId).messageId) {
             networkDatabase.at(incoming.nodeId) = incoming;
+            needsPersistence = true;   // mark we want to save it eventually
             Serial.printf("DB: Node %d updated (Msg %d)\n", incoming.nodeId, incoming.messageId);
         } 
         else {
@@ -45,6 +50,48 @@ inline String getDatabaseAsJson() {
     String output;
     serializeJson(doc, output);
     return output;
+}
+
+// retrieves data from backup in LittleFS
+inline void getDatabaseFromFS() {
+    // check backup file exists
+    if (!LittleFS.exists("/db_backup.json")) {
+        Serial.println("DB: No backup file found, starting fresh.");
+        return;
+    }
+
+    // ensure you can open it to read
+    File file = LittleFS.open("/db_backup.json", "r");
+    if (!file) {
+        Serial.println("Failed to open file for reading.");
+        return;
+    }
+
+    // deserialize json file
+    JsonDocument doc;
+    DeserializationError error = deserializeJson(doc, file);
+    file.close();
+
+    if (error) {
+        Serial.print("DB: Deserialization failed: ");
+        Serial.println(error.c_str());
+        return;
+    }
+
+    // convert to our C++ struct
+    JsonArray array = doc.as<JsonArray>();
+    for (JsonObject obj : array) {
+        uint32_t id = obj["id"];
+        if (id < MAX_NODES) {
+            networkDatabase[id].nodeId = id;
+            networkDatabase[id].messageId = obj["mId"];
+            networkDatabase[id].batteryVoltage = obj["batt"];
+            networkDatabase[id].motionDetected = obj["motion"];
+            networkDatabase[id].doorOpen = obj["door"];
+            strlcpy(networkDatabase[id].nodeName, obj["name"], sizeof(networkDatabase[id].nodeName));
+        }
+    }
+    Serial.println("DB: Database restored.");
 }
 
 #endif
