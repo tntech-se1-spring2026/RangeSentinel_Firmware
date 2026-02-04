6417dec9f11f6b7c3e10833949fa3a0db83b25da
Syndr <syndr@goose-games.com>
2026-02-03 19:58:41 -0600

    refactor: change generic struct declaration to a typedef

--- include/shared_types.h
+++ include/shared_types.h
@@ -6,14 +6,14 @@
 
 // database schema to hold a node's status
 // subject to change based on what we want
-struct NodeStatus {
+typedef struct {
     uint32_t nodeId;
     uint32_t messageId;  // to prevent duplicate alerts if both sensors register
     float batteryVoltage;  // can calculate percentage left
     bool motionDetected;
     bool doorOpen;
     char nodeName[20];
-};
+} NodeStatus;
 
 // translate C++ struct to JSON
 inline String nodeStatusToJson(const NodeStatus& status) {
