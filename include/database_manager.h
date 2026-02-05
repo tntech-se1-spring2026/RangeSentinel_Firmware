d63361e90f8832eca4a57aa9c89db21c92e57f57
Andrew Johnson <andrewjohnson11235@gmail.com>
2026-02-05 14:04:25 -0600

    feat: Add function to get persistent history in getDatabaseFromFS(). Minor fixes

--- include/database_manager.h
+++ include/database_manager.h
@@ -94,12 +94,12 @@ inline bool saveDatabaseToFS() {
             }
         }
         if (serializeJson(doc, file) == 0) {
-            Serial.println("Failed to write current status to DB file.");
+            Serial.println("DB: Failed to write current status to DB file.");
         }
         file.close();
     } 
     else {
-        Serial.println("Failed to open DB file for writing.");
+        Serial.println("DB: Failed to open DB file for writing.");
         return false;
     }
 
@@ -108,7 +108,7 @@ inline bool saveDatabaseToFS() {
     if (logFile) {
         JsonDocument logDoc;
         logDoc["head"] = logHead;  // save position in circular buffer
-        JsonArray logs = logDoc["data"].add<JsonArray>();  // add label since we have head as well
+        JsonArray logs = logDoc["data"].to<JsonArray>();  // add label since we have head as well
 
         for (const auto& entry : eventLog) {
             if (entry.nodeId > 0) {
@@ -123,61 +123,91 @@ inline bool saveDatabaseToFS() {
             }
         }
         if (serializeJson(logDoc, logFile) == 0) {
-            Serial.println("Failed to write to log file.");
+            Serial.println("DB: Failed to write to log file.");
         }
         logFile.close();
     }
     else {
-        Serial.println("Failed to open history file for writing");
+        Serial.println("DB: Failed to open log file for writing");
         return false;
     }
 
     needsPersistence = false;
-    Serial.println("Database successfully backed to LittleFS.");
+    Serial.println("DB: Database and log file successfully backed to LittleFS.");
     return true;
 }
 
 // retrieves data from backup in LittleFS
 inline void getDatabaseFromFS() {
-    // check backup file exists
-    if (!LittleFS.exists("/db_backup.json")) {
+    // restore current network status
+    if (LittleFS.exists("/db_backup.json")) {
+        File file = LittleFS.open("/db_backup.json", "r");
+        if (file) {
+            JsonDocument doc;
+            DeserializationError error = deserializeJson(doc, file);
+            if (!error) {
+                JsonArray array = doc.as<JsonArray>();
+                for (JsonObject obj : array) {
+                    uint32_t id = obj["id"];
+                    if (id < MAX_NODES) {
+                        networkDatabase[id].nodeId = id;
+                        networkDatabase[id].messageId = obj["mId"];
+                        networkDatabase[id].batteryVoltage = obj["batt"];
+                        networkDatabase[id].motionDetected = obj["motion"];
+                        networkDatabase[id].doorOpen = obj["door"];
+                        strlcpy(networkDatabase[id].nodeName, obj["name"], sizeof(networkDatabase[id].nodeName));
+                        networkDatabase[id].lastSeen = obj["ls"];
+                    }
+                }
+                Serial.println("DB: Live status restored.");
+            }
+            else {
+                Serial.println("DB: Deserialization error in db_backup.json");
+                return;
+            }
+            file.close();
+        }
+    }
+
+    // restore circular history log
+    if (LittleFS.exists("/history_backup.json")) {
+        File logFile = LittleFS.open("/history_backup.json", "r");
+        if (logFile) {
+            JsonDocument logDoc;
+            DeserializationError error = deserializeJson(logDoc, logFile);
+            if (!error) {
+                logHead = logDoc["head"] | 0;   // so we know where to resume. defualt to 0
+                JsonArray logs = logDoc["data"].as<JsonArray>();
+                int i = 0;
+                for (JsonObject obj : logs) {
+                    if (i < MAX_LOG_ENTRIES) {
+                        eventLog[i].nodeId = obj["id"];
+                        eventLog[i].messageId = obj["mId"];
+                        eventLog[i].batteryVoltage = obj["batt"];
+                        eventLog[i].motionDetected = obj["motion"];
+                        eventLog[i].doorOpen = obj["door"];
+                        eventLog[i].lastSeen = obj["ls"];
+                        strlcpy(eventLog[i].nodeName, obj["name"] | "Unknown", sizeof(eventLog[i].nodeName));
+                        i++;
+                    }
+                }
+                Serial.println("DB: History log restored.");
+            }
+            else {
+                Serial.println("DB: Deserialization error in history_backup.json");
+                return;
+            }
+            logFile.close();
+        }
+        else {
+            Serial.println("DB: Failed to open file for reading.");
+            return;
+        }
+    }
+    else {
         Serial.println("DB: No backup file found, starting fresh.");
         return;
     }
-
-    // ensure you can open it to read
-    File file = LittleFS.open("/db_backup.json", "r");
-    if (!file) {
-        Serial.println("Failed to open file for reading.");
-        return;
-    }
-
-    // deserialize json file
-    JsonDocument doc;
-    DeserializationError error = deserializeJson(doc, file);
-    file.close();
-
-    if (error) {
-        Serial.print("DB: Deserialization failed: ");
-        Serial.println(error.c_str());
-        return;
-    }
-
-    // convert to our C++ struct
-    JsonArray array = doc.as<JsonArray>();
-    for (JsonObject obj : array) {
-        uint32_t id = obj["id"];
-        if (id < MAX_NODES) {
-            networkDatabase[id].nodeId = id;
-            networkDatabase[id].messageId = obj["mId"];
-            networkDatabase[id].batteryVoltage = obj["batt"];
-            networkDatabase[id].motionDetected = obj["motion"];
-            networkDatabase[id].doorOpen = obj["door"];
-            strlcpy(networkDatabase[id].nodeName, obj["name"], sizeof(networkDatabase[id].nodeName));
-            networkDatabase[id].lastSeen = obj["ls"];
-        }
-    }
-    Serial.println("DB: Database restored.");
 }
 
 // converts circular event log to json
