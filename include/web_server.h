4e2d6c5c348a51978e84635a40caab8d443326a4
Syndr <syndr@goose-games.com>
2026-02-06 12:30:46 -0600

    Fix merge conflict

--- include/web_server.h
+++ include/web_server.h
@@ -6,27 +6,6 @@
 // officially declared in main.cpp
 extern WebServer server;
 
-<<<<<<< HEAD
-void setupWebServer(String (*getStatusJson)()) {
-    // home page
-    server.on("/", HTTP_GET, []() {
-        File file = LittleFS.open("/www/index.html", "r");
-        if (!file) {
-            server.send(404, "text/plain", "Web dashboard file not found in LittleFS");
-            return;
-        }
-        server.streamFile(file, "text/html");
-    });
-
-    // JSON data api
-    server.on("/api/status", HTTP_GET, [getStatusJson]() {
-        server.send(200, "application/json", getStatusJson());
-    });
-    server.begin();
-    Serial.println("HTTP server started");
-}
-=======
 void setupWebServer(String (*getStatusJson)(), String (*getLogJson)());
->>>>>>> dev
 
 #endif
