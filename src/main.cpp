8d86850451996d6c16a400b1a02ec3227d23851f
Syndr <syndr@goose-games.com>
2026-02-12 19:31:05 -0600

    Add captive dns server functionality

--- src/main.cpp
+++ src/main.cpp
@@ -23,9 +23,13 @@ void loop() {
 #include <WiFi.h>
 #include <WebServer.h>
 #include <LittleFS.h>
+#include <DNSServer.h>
 
 // standard HTTP port
 WebServer server(80);
+
+DNSServer dnsServer;
+#define DNS_PORT 53
 
 unsigned long previousMillis = 0;
 const long interval = 300000; // 5 minutes
@@ -48,11 +52,14 @@ void setup() {
     Serial.print("Access IP Address: ");
     Serial.println(WiFi.softAPIP());  // should default to 192.168.4.1
 
+    dnsServer.start(DNS_PORT, "*", WiFi.softAPIP());
+
     setupWebServer(getDatabaseAsJson, getEventLogAsJson);
 }
 
 void loop() {
     server.handleClient();
+    dnsServer.processNextRequest();
 
     // periodic saving
     unsigned long currentMillis = millis();
