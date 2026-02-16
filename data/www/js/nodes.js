5d7a3f5f269c56f1d827551f1f5f85ea2e29b60c
Daniel <dgeer0120@gmail.com>
2026-02-16 09:16:50 -0600

    Added a refresh of the cards and edit the card creator

--- data/www/js/nodes.js
+++ data/www/js/nodes.js
@@ -1,5 +1,6 @@
 import * as query from "/modules/query.js";
 //Manual card creator for testing purposes
+/*
 let count = 1; //Card number
 const cardRow = document.getElementById("cardRow"); // Get the card row container
 document.querySelector('#test-button').addEventListener("click",async () => { //Listen for button click to add card
@@ -9,34 +10,81 @@ document.querySelector('#test-button').addEventListener("click",async () => { //
     )
     count++;
 });
-function card_creator(id, name, lastSeen){ //Template for card creation
-    if (lastSeen >= 10000){ //If not seen for more than 10 mins, mark as offline
-        return    `
-            <div class="col-12 col-md-4">
-                <div class="card text-center shadow-sm">
-                    <div class="card-body">
-                        <h5 class="card-title">${name}</h5>
-                        <h3 class="card-title">${id}</h3>
-                        <p class="card-value text-danger">Offline</p>
-                        <p class="card-text text-muted">${lastSeen}</p>
-                    </div>
-                </div>
-            </div>`;
+
+async function fetch_data(){
+    const nodes_data_promise = await query.get_nodes();
+    const node_data = await nodes_data_promise.json();
+    console.log(node_data);
+    let node_network = [];
+    for (const iter in node_data["nodes"]){
+        console.log(node_data["nodes"][iter]);
+        let id_name =node_data["nodes"][iter]["id"];
+        let condition = check_time(node_data["nodes"][iter]["lastSeen"]);
+        let alert_status = node_data["nodes"][iter]["alert_status"]
+        let single_node = [id_name,condition,alert_status];
+        node_network.push(single_node);
     }
-    else{ //Otherwise, mark as online
-        return    `
-            <div class="col-12 col-md-4">
-                <div class="card text-center shadow-sm">
-                    <div class="card-body">
-                        <h5 class="card-title">${name}</h5>
-                        <h3 class="card-title">${id}</h3>
-                        <p class="card-value text-success">Online</p>
-                        <p class="card-text text-muted">${lastSeen}</p>
-                    </div>
-                </div>
-            </div>`;
+    return node_network; //Returns an array of array of the nodes and its id and conditon
+}
+*/
+async function alert_check(){
+    let alerted_nodes = [];
+    let refresh_page = false;
+    for (let i = 0; i <= node_network.length;i++){
+        if(node_network[i][2] == true){
+            alerted_nodes.push(node_network[i]);
+            refresh_page = true;
+        }
     }
 }
+//Types of notifications
+/*
+
+*/
+//Refresh Data
+const time = 1000000;
+async function refresh_cards(){
+    const cardRow = document.getElementById("cardRow");
+    cardRow.innerHTML ="";
+    const nodes_data_promise = await query.get_nodes();
+    const node_data = await nodes_data_promise.json();
+    for (const iter in node_data["nodes"]){
+        console.log(node_data["nodes"][iter]);
+        let id_name =node_data["nodes"][iter]["id"];
+        let condition = check_time(node_data["nodes"][iter]["lastSeen"]);
+        let alert_status = node_data["nodes"][iter]["alert_status"];
+        card_creator(id_name,alert_status,condition);
+    }
+    
+}
+//Refresh the data every 10 minutes
+setTimeout(()=>{
+    refresh_cards();
+},1000000) //10 min
+
+function card_creator(id, name, lastSeen){ //Template for card creation
+    const current_time = Date.now();
+    const time_difference = current_time - lastSeen
+    if (time_difference >= 1000000){  //If not seen for more than 10 mins, mark as offline
+        let online_status = "Offline";
+        let text_style = "text-danger";
+    }
+    else{
+        let online_status = "Online";   //Otherwise, mark as online
+        let text_style = "text-success";
+    }
+    return    `
+        <div class="col-12 col-md-4">
+            <div class="card text-center shadow-sm">
+                <div class="card-body">
+                    <h5 class="card-title">${name}</h5>
+                    <h3 class="card-title">${id}</h3>
+                    <p class="card-value ${text_style}">${online_status}</p>
+                    <p class="card-text text-muted">${lastSeen}</p>
+                </div>
+            </div>
+        </div>`;
+    }
 //Automatically load cards on page load from data in the backend
 document.addEventListener("DOMContentLoaded", nodeCall);
 function nodeCall(){
