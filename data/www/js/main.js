a1695958b1504f6e6134cf9aabf7b1d328a06dad
jwthomas44 <helpedpig831@gmail.com>
2026-02-23 11:44:37 -0600

    Laid out cards properly and made it so when you click the notification clear button, it clears the associated notif in the dropdown.

--- data/www/js/main.js
+++ data/www/js/main.js
@@ -1,12 +1,21 @@
 import * as nodes from "/modules/nodes.js";
 
-document.addEventListener("DOMContentLoaded", updateAll);
-document.querySelector("#notif-button").addEventListener("click", testNotification);
-document.addEventListener("click", function(event) {
+document.addEventListener("DOMContentLoaded", updateAll); //Loads all nodes when page is loaded
+document.querySelector("#notif-button").addEventListener("click", testNotification); //Tests notification retrieval when button is clicked
+document.addEventListener("click", function(event) { //Event delegation for alert clear buttons
     if (event.target.classList.contains("alert-clear")) {
         const card = event.target.closest('.card');
         card.classList.remove('bg-warning');
         card.classList.add('bg-light');
+        const nodeId = card.id;
+        const notification = document.querySelector(
+            `.dropdown-item[data-node-id="${nodeId}"]`
+        );
+
+        if (notification) {
+            console.log(`Clearing notification for node ${nodeId}`);
+            notification.remove();
+        }
     }
 });
 
